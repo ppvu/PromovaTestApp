6f366c1c70a9a019fcfe3f9b645f941ca4a87659
Yevhen Kononenko <areyouready575@gmail.com>
2024-01-21 12:38:35 +0200

    Initial Commit

---
+++ PromovaTestApp/PromovaTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromovaTestAppApp.swift
+//  PromovaTestApp
+//
+//  Created by Yevhen Kononenko on 21.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct PromovaTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
